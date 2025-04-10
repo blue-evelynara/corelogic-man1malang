25d6636b5092595d39cc44c495e9fa5e22f8b845
blue-evelynara <rvln.velyna@gmail.com>
2025-04-10 16:14:09 +0700

    streamlit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Portfoilo")
+st.write("Welcome to this portfolio")
+
+container = st.container()
+with container:
+    st.subheader("About")
+    c.write("User is....")
+    c.write("user also...")
